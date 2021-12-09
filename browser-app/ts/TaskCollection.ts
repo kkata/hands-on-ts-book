06f5a2e17056dc241b48d3dfdc7873fc4f420530
kkata <14849535+kkata@users.noreply.github.com>
2021-12-09 16:19:18 +0900

    019 データの永続化 実装

--- browser-app/ts/TaskCollection.ts
+++ browser-app/ts/TaskCollection.ts
@@ -1,16 +1,19 @@
 import { Status, Task } from "./Task";
 
+const STORAGE_KEY = "TASK";
+
 export class TaskCollection {
   private readonly storage;
   private tasks: Task[] = [];
 
   constructor() {
     this.storage = localStorage;
-    console.log(this.storage);
+    this.tasks = this.getStoredTasks();
   }
 
   add(task: Task) {
     this.tasks.push(task);
+    this.updateStorage();
   }
 
   delete(task: Task) {
@@ -26,9 +29,31 @@ export class TaskCollection {
       if (item.id === task.id) return task;
       return item;
     });
+    this.updateStorage();
   }
 
   fileter(filterStatus: Status) {
     return this.tasks.filter(({ status }) => status === filterStatus);
+  }
+
+  private updateStorage() {
+    this.storage.setItem(STORAGE_KEY, JSON.stringify(this.tasks));
+  }
+
+  private getStoredTasks() {
+    const jsonString = this.storage.getItem(STORAGE_KEY);
+
+    if (!jsonString) return [];
+
+    try {
+      const storedTasks: any[] = JSON.parse(jsonString);
+      const tasks = storedTasks.map((task) => new Task(task));
+
+      console.log(tasks);
+      return tasks;
+    } catch {
+      this.storage.removeItem(STORAGE_KEY);
+      return [];
+    }
   }
 }
