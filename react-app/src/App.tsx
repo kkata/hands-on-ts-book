b090cc1dc806589b5a321e000da58a4ed09fa1c6
kkata <14849535+kkata@users.noreply.github.com>
2021-12-19 18:26:41 +0900

    023 Buttonコンポーネント

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,14 @@
 import { Text } from './libs/Text'
 import { Heading } from './libs/Heading'
+import { Button, AlertButton } from './libs/Button'
 
 export const App = () => {
   return (
     <>
+      <Button onClick={() => console.log('clicked!')} title="Button" type="primary" width={96} />
+      <Button onClick={() => console.log('clicked!')} title="Button" type="secondary" />
+      <Button onClick={() => console.log('clicked!')} title="Button" type="error" />
+      <AlertButton onClick={() => console.log('clicked!')} title="Button" />
       <Heading tag="h1">見出し</Heading>
       <Heading tag="h1">
         <span>hello, world!</span>
