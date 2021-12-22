f62cc45ca4e4e039b498f3fcdfa08d7473b6f1dc
kkata <14849535+kkata@users.noreply.github.com>
2021-12-22 20:52:24 +0900

    023 Inputコンポーネント

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,10 +2,12 @@ import { Text } from './libs/Text'
 import { Heading } from './libs/Heading'
 import { Button, AlertButton } from './libs/Button'
 import { Textarea } from './libs/Textarea'
+import { Input } from './libs/Input'
 
 export const App = () => {
   return (
     <>
+      <Input type="text" />
       <Textarea width={500} maxLength={10} />
       <Button onClick={() => console.log('clicked!')} title="Button" type="primary" width={96} />
       <Button onClick={() => console.log('clicked!')} title="Button" type="secondary" />
